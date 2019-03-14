bca08930e08b38180fcdd270dc2a4e6f246c616e
mac <mac@macdeMac-mini.local>
2019-03-14 18:24:44 +0800

    fix for new version of react native

--- ios/RNLockState.h
+++ ios/RNLockState.h
@@ -1,7 +1,11 @@
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
+#else
+#import "RCTBridgeModule.h"
+#import "RCTEventEmitter.h"
+#endif
 
-@interface RNLockState : RCTEventEmitter
+@interface RNLockState : RCTEventEmitter <RCTBridgeModule>
 
 @end
-  
